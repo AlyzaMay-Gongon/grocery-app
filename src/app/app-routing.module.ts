5b5e0989b161da3e9069e8ce9ae56bf31cc69708
Alyza May Gongon <alyzamay.gongon@g.batstate-u.edu.ph>
2021-07-08 17:31:22 +0800

    Try lang natin mga bes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '',redirectTo: 'folder/Inbox',pathMatch: 'full'},
+  {path: 'folder/:id',loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)},
+  {path: 'loader',loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)},
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 
